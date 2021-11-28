74aa1af11f61767da34f4f8fb1b0a2a1f89876de
itpark-team <46758840+itpark-team@users.noreply.github.com>
2021-11-28 18:32:36 +0300

    Добавьте файлы проекта.

---
+++ FirstHomeWork/PhoneNumbersManager.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FirstHomeWork
+{
+    class PhoneNumbersManager
+    {
+        private List<PhoneNumber> phoneNumbers;
+        private const string fileNameTXT = "PhoneListNumbers.txt";
+        private const string fileNameBIN = "PhoneListNumbers.bin";
+
+        public PhoneNumbersManager()
+        {
+            phoneNumbers = new List<PhoneNumber>();
+        }
+
+        public void AddPhoneNumber(PhoneNumber phoneNumber)
+        {
+            phoneNumbers.Add(phoneNumber);
+        }
+
+        public bool ReplacePhoneNumber(int index, PhoneNumber phoneNumber)
+        {
+            if (index >= 0 && index <= phoneNumbers.Count - 1)
+            {
+                phoneNumbers[index] = phoneNumber;
+                return true;
+            }
+            else
+            {
+                throw new Exception("Такого контакта не существует");
+            }
+
+        }
+
+        public bool DeletePhoneNumber(int index)
+        {
+            if (index >= 0 && index <= phoneNumbers.Count - 1)
+            {
+                phoneNumbers.RemoveAt(index);
+                return true;
+            }
+            else
+            {
+                throw new Exception("такого контакта не существует");
+            }
+        }
+
+        public void PrintPhoneNumbers()
+        {
+            if (phoneNumbers.Count == 0)
+            {
+                Console.WriteLine("\nЧто ж, список контактов пуст :(");
+                Console.WriteLine("======");
+            }
+            else
+            {
+                for (int i = 0; i < phoneNumbers.Count; i++)
+                {
+                    Console.WriteLine($"Котнтакт номер №{i + 1}");
+                    Console.WriteLine(phoneNumbers[i]);
+                    Console.WriteLine("\n+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+                }
+            }
+        }
+
+        public void PrintPhoneNumbersByGrade(PhoneNumber.GradeList grade)
+        {
+            int count = 0;
+
+            for (int i = 0; i < phoneNumbers.Count; i++)
+            {
+                if (phoneNumbers[i].Grade == grade)
+                {
+                    count++;
+                    Console.WriteLine($"Котнтакт номер №{i + 1}");
+                    Console.WriteLine(phoneNumbers[i]);
+                    Console.WriteLine("\n+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+                }
+            }
+
+            if (count == 0)
+            {
+                Console.WriteLine("\nЧто ж, никого с заданными парметрами не нашлось :(");
+            }
+        }
+
+
+        public void LoadPhoneNumbersFromTxtFile()
+        {
+            StreamReader reader = new StreamReader(fileNameTXT);
+
+            int count = Convert.ToInt32(reader.ReadLine());
+            phoneNumbers = new List<PhoneNumber>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string number = reader.ReadLine();
+                string name = reader.ReadLine();
+                string info = reader.ReadLine();
+                PhoneNumber.GradeList grade = (PhoneNumber.GradeList)int.Parse(reader.ReadLine());
+
+                phoneNumbers.Add(new PhoneNumber(number, name, info, grade));
+            }
+
+            reader.Close();
+        }
+
+        public void SavePhoneNumbersTxtToFile()
+        {
+            StreamWriter writer = new StreamWriter(fileNameTXT);
+            int count = phoneNumbers.Count;
+            writer.WriteLine(count);
+
+            for (int i = 0; i < phoneNumbers.Count; i++)
+            {
+                writer.WriteLine(phoneNumbers[i].Number);
+                writer.WriteLine(phoneNumbers[i].Name);
+                writer.WriteLine(phoneNumbers[i].Info);
+                writer.WriteLine((int)phoneNumbers[i].Grade);
+            }
+
+            writer.Close();
+        }
+
+        public void LoadPhoneNumbersFromBinFile()
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            FileStream fileStream = new FileStream(fileNameBIN, FileMode.Open);
+
+            phoneNumbers = (List<PhoneNumber>)binaryFormatter.Deserialize(fileStream);
+
+            fileStream.Close();
+        }
+
+        public void SavePhoneNumbersBinToFile()
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            FileStream fileStream = new FileStream(fileNameBIN, FileMode.OpenOrCreate);
+
+            binaryFormatter.Serialize(fileStream, phoneNumbers);
+
+            fileStream.Close();
+        }
+    }
+
+}
---
+++ FirstHomeWork/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FirstHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneNumbersManager phmanager = new PhoneNumbersManager();
+            int action;
+
+            /*PhoneNumber Pnum1 = new PhoneNumber("99999", "Катя", "Шлюха", PhoneNumber.GradeList.important);
+            PhoneNumber Pnum2 = new PhoneNumber("88888", "Анжелла", "Шлюха но получше", PhoneNumber.GradeList.friend);
+
+            phmanager.AddPhoneNumber(Pnum1);
+            phmanager.AddPhoneNumber(Pnum2);*/
+
+            while (true)
+            {
+                Console.Clear();
+                phmanager.PrintPhoneNumbers();
+
+                Console.WriteLine("Меню контактов");
+                Console.WriteLine("1. Добавить контакт");
+                Console.WriteLine("2. Удалить контакт");
+                Console.WriteLine("3. Изменить контакт");
+                Console.WriteLine("4. Сохранить контакты в txt файл");
+                Console.WriteLine("5. Загрузить контакты из txt файла");
+                Console.WriteLine("6. Сохранить контакты в bin файл");
+                Console.WriteLine("7. Загрузить контакты из bin файла");
+                Console.WriteLine("8. Выбрать контакты по типу важности");
+                Console.WriteLine("0. Выход");
+                Console.Write("Введите действие: ");
+                action = int.Parse(Console.ReadLine());
+
+                switch (action)
+                {
+                    case 1:
+                        {
+                            Console.Write("Введите номер телефона: ");
+                            string number = Console.ReadLine();
+                            Console.Write("Введите имя контакта: ");
+                            string name = Console.ReadLine();
+                            Console.Write("Введите информацию о контакте: ");
+                            string info = Console.ReadLine(); 
+                            Console.Write("Важность контакта/кем является контакт\n0 - коллега,\n1 - друг,\n2 - родственник,\n3 - важный контакт: ");
+                            PhoneNumber.GradeList grade = (PhoneNumber.GradeList)int.Parse(Console.ReadLine());
+
+                            PhoneNumber pnum = new PhoneNumber(number, name, info, grade);
+
+                            phmanager.AddPhoneNumber(pnum);
+                        }
+                        break;
+                    case 2:
+                        {
+                            try
+                            {
+                                Console.Write("Введите индекс контакта : ");
+                                int index = int.Parse(Console.ReadLine());
+
+                                Console.WriteLine(phmanager.DeletePhoneNumber(index - 1));
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+
+                        }
+                        break;
+                    case 3:
+                        {
+                            try
+                            {
+                                Console.Write("Введите индекс контакта : ");
+                                int index = int.Parse(Console.ReadLine());
+
+                                Console.Write("Введите номер телефона: ");
+                                string number = Console.ReadLine();
+                                Console.Write("Введите имя контакта: ");
+                                string name = Console.ReadLine();
+                                Console.Write("Введите информацию о контакте: ");
+                                string info = Console.ReadLine();
+                                Console.Write("Важность контакта/кем является контакт\n0 - коллега,\n1 - друг,\n2 - родственник,\n3 - важный контакт: ");
+                                PhoneNumber.GradeList grade = (PhoneNumber.GradeList)int.Parse(Console.ReadLine());
+
+                                PhoneNumber pnum = new PhoneNumber(number, name, info, grade);
+
+                                phmanager.ReplacePhoneNumber(index - 1, pnum);
+
+                                Console.WriteLine("Замена выполнена");
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+
+                        }
+                        break;
+                    case 4:
+                        {
+                            phmanager.SavePhoneNumbersTxtToFile();
+                            Console.WriteLine("успешно сохранено");
+
+                        }
+                        break;
+                    case 5:
+                        {
+                            phmanager.LoadPhoneNumbersFromTxtFile();
+                            Console.WriteLine("успешно загружено");
+                        }
+                        break;
+
+                    case 6:
+                        {
+                            phmanager.SavePhoneNumbersBinToFile();
+                            Console.WriteLine("успешно сохранено");
+
+                        }
+                        break;
+                    case 7:
+                        {
+                            phmanager.LoadPhoneNumbersFromBinFile();
+                            Console.WriteLine("успешно загружено");
+                        }
+                        break;
+
+                    case 0:
+                        {
+                            Environment.Exit(0);
+                        }
+                        break;
+                    case 8:
+                        {
+                            Console.Write("Важность контакта/кем является контакт\n0 - коллега,\n1 - друг,\n2 - родственник,\n3 - важный контакт: ");
+                            PhoneNumber.GradeList grade = (PhoneNumber.GradeList)int.Parse(Console.ReadLine());
+
+                            phmanager.PrintPhoneNumbersByGrade(grade);
+                        }
+                        break;
+
+                    default:
+                            Console.WriteLine("Такого действия нету!");
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
